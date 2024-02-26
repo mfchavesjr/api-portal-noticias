0bbd5bf402989c93615da4dee6d45a9cacbb70a0
Mauricio <feira.nova@gmail.com>
2024-02-26 16:37:59 -0300

    Refatoração dos recursos de UsuarioCreate, utilizando DTOs

--- src/main/java/com/mchaves/apiadmnews/web/conroller/UsuarioController.java
+++ src/main/java/com/mchaves/apiadmnews/web/conroller/UsuarioController.java
@@ -2,6 +2,9 @@ package com.mchaves.apiadmnews.web.conroller;
 
 import com.mchaves.apiadmnews.entity.Usuario;
 import com.mchaves.apiadmnews.service.UsuarioService;
+import com.mchaves.apiadmnews.web.dto.UsuarioCreateDto;
+import com.mchaves.apiadmnews.web.dto.UsuarioResponseDto;
+import com.mchaves.apiadmnews.web.dto.mapper.UsuarioMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,9 +26,9 @@ public class UsuarioController {
     }
 
   @PostMapping
-    public ResponseEntity<Usuario> create(@RequestBody Usuario usuario){
-         Usuario user = usuarioService.salvar(usuario);
-        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    public ResponseEntity<UsuarioResponseDto> create(@RequestBody UsuarioCreateDto createDto){
+         Usuario user = usuarioService.salvar(UsuarioMapper.toUsuario(createDto));
+        return ResponseEntity.status(HttpStatus.CREATED).body(UsuarioMapper.toDto(user));
     }
 
     @GetMapping("/{id}")
