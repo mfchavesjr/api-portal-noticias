6ee247b30cedc6160ef68675a36f002f3bcaa4bd
Mauricio <feira.nova@gmail.com>
2024-02-26 09:46:00 -0300

    first commit

---
+++ src/main/java/com/mchaves/apiadmnews/ApiAdmNewsApplication.java
@@ -0,0 +1,13 @@
+package com.mchaves.apiadmnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAdmNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAdmNewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mchaves/apiadmnews/ApiAdmNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mchaves.apiadmnews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAdmNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
