4f824bd45430bb0aeb81ebe9b56db61b1560e6ee
Mauricio <feira.nova@gmail.com>
2024-02-26 11:04:45 -0300

    Criação da classe Usuario Controller

---
+++ src/main/java/com/mchaves/apiadmnews/web/conroller/UsuarioController.java
@@ -0,0 +1,14 @@
+package com.mchaves.apiadmnews.web.conroller;
+
+import com.mchaves.apiadmnews.service.UsuarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("api/v1/usuarios")
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+}
