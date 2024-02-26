140f82af490ca8904a64ce142b81b9a3bf9dd472
Mauricio <feira.nova@gmail.com>
2024-02-26 11:44:36 -0300

    Implementação do recurso Listar todos os usuários

--- src/main/java/com/mchaves/apiadmnews/web/conroller/UsuarioController.java
+++ src/main/java/com/mchaves/apiadmnews/web/conroller/UsuarioController.java
@@ -7,12 +7,20 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api/v1/usuarios")
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> getAll() {
+        List<Usuario> users = usuarioService.buscarTodos();
+        return ResponseEntity.ok(users);
+    }
 
   @PostMapping
     public ResponseEntity<Usuario> create(@RequestBody Usuario usuario){
