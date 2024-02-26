db94622da78c3cb5666d9d6a3e32754d2e362f13
Mauricio <feira.nova@gmail.com>
2024-02-26 11:00:30 -0300

    Criação da classe Usuario Service e o serviço método BuscarTodos

--- src/main/java/com/mchaves/apiadmnews/service/UsuarioService.java
+++ src/main/java/com/mchaves/apiadmnews/service/UsuarioService.java
@@ -1,4 +1,21 @@
 package com.mchaves.apiadmnews.service;
 
+import com.mchaves.apiadmnews.entity.Usuario;
+import com.mchaves.apiadmnews.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
 public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    @Transactional(readOnly = true)
+    public List<Usuario> buscarTodos() {
+        return usuarioRepository.findAll();
+    }
 }
