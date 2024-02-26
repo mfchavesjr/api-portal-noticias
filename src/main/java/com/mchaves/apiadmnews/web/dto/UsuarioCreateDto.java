965630a74cfa8282c4db35a188da9209dbf4aa27
Mauricio <feira.nova@gmail.com>
2024-02-26 16:16:48 -0300

    Implantação do pacote Validation, Criação das classes de DTO: UsuarioCreateDto e UsuarioResponseDto

---
+++ src/main/java/com/mchaves/apiadmnews/web/dto/UsuarioCreateDto.java
@@ -0,0 +1,19 @@
+package com.mchaves.apiadmnews.web.dto;
+
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class UsuarioCreateDto {
+
+    @NotBlank
+    @Email(message = "formato do e-mail está invalido", regexp = "^[a-z0-9.+-]+@[a-z0-9.-]+\\.[a-z]{2,}$")
+    private String username;
+    @NotBlank
+    @Size(min = 6, max = 6)
+    private String password;
+}
